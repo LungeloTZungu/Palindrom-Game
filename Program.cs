e6c2b76c93958bdcc0ac85c6e51f841a1d28dd63
LungeloTZungu <lzungu56@gmail.com>
2023-08-15 20:34:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace PalindromGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word: ");
+            string reversWord = "";
+            string word = Console.ReadLine();
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversWord += word[i];
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("Original word:");
+
+            Console.WriteLine(word);
+
+            Console.WriteLine(" ");
+
+            Console.Write("Reversed word:");
+
+            Console.Write(reversWord +"\n");
+
+
+            Console.WriteLine(" ");
+
+            if (reversWord.Equals(word))
+            {
+                Console.Write("This is a palindrom");
+            }
+            else
+            {
+                Console.Write("This is not a palindrom");
+
+            }
+
+        }
+    }
+}
